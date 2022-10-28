3f86ac04d71edeac34406fd7dddda4d519f3432e
david.cheung <kccheung0625@gmail.com>
2022-10-28 10:46:00 +0800

    modified:   binance/streams.py

--- binance/streams.py
+++ binance/streams.py
@@ -144,11 +144,10 @@ class ReconnectingWebsocket:
             try:
                 if self._queue.empty():
                     return [await asyncio.wait_for(self._queue.get(), timeout=self.TIMEOUT, loop=self._loop)]
-                elif self._queue.qsize() == 1:
-                    return [self._queue.get_nowait()]
                 else:
-                    msgs = list(self._queue._queue)
-                    self._queue = asyncio.Queue(loop=self._loop)
+                    msgs = []
+                    for _ in range(self._queue.qsize()):
+                        msgs.append(self._queue.get_nowait())
                     return msgs
             except asyncio.TimeoutError:
                 self._log.debug(f"no message in {self.TIMEOUT} seconds")
