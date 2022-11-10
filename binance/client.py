b0cdd8257e61eb22721da4125c9d1f0800e05955
david.cheung <kccheung0625@gmail.com>
2022-11-10 10:57:10 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -19,8 +19,8 @@ from yarl import URL
 class BaseClient:
     BASE_API_URLS = ['https://api.binance.com',
                      'https://api1.binance.com',
-                     'https://api2.binance.com',
-                     'https://api3.binance.com']
+                     'https://api2.binance.com']
+                     # 'https://api3.binance.com']
     API_URL = 'https://api.binance.{}/api'
     API_TESTNET_URL = 'https://testnet.binance.vision/api'
     MARGIN_API_URL = 'https://api.binance.{}/sapi'
