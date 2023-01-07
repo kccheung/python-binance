553181e1f51a473b9de4a763499eaf7bfb3dbfba
david.cheung <kccheung0625@gmail.com>
2023-01-08 00:13:15 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -233,7 +233,7 @@ class BaseClient:
         return m.hexdigest()
 
     def _rsa(self, msg) -> str:
-        return b64encode(pkcs1_15.new(self.API_SECRET).sign(SHA256.new(msg.encode('utf-8')))).replace(b'=', b'%3D').replace(b'/', b'%2F').replace(b'+', b'%2B')
+        return b64encode(pkcs1_15.new(self.API_SECRET).sign(SHA256.new(msg.encode('utf-8')))).decode().replace('=', '%3D').replace('/', '%2F').replace('+', '%2B')
 
     def _generate_signature(self, data: Dict) -> str:
         ordered_data = self._order_params(data)
