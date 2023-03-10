9e78d88ae4972ce3797341bca1966c30c41bce3f
david.cheung <kccheung0625@gmail.com>
2023-03-10 17:21:28 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -22,11 +22,13 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 
 class BaseClient:
-    BASE_API_URLS = ['https://api.binance.com',
-                     'https://api1.binance.com',
-                     'https://api2.binance.com',
-                     'https://api3.binance.com',
-                     'https://api4.binance.com']
+    BASE_API_URLS = [
+        # 'https://api.binance.com',
+        # 'https://api1.binance.com',
+        # 'https://api2.binance.com',
+        'https://api3.binance.com',
+        'https://api4.binance.com'
+    ]
     API_URL = 'https://api.binance.{}/api'
     API_TESTNET_URL = 'https://testnet.binance.vision/api'
     MARGIN_API_URL = 'https://api.binance.{}/sapi'
