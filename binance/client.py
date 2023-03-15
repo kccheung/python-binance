c00ffc45b913043341e18a2882bc28f73f9abd7a
david.cheung <kccheung0625@gmail.com>
2023-03-15 11:05:40 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -589,7 +589,7 @@ class Client(BaseClient):
     def reset_timestamp_offset(self):
         send_time_local = time.time_ns()
         receive_time_server = self.get_server_time_fast()
-        self.timestamp_offset = -int((time.time_ns() + send_time_local) / 2000000.0) + receive_time_server['serverTime']
+        self.timestamp_offset = -math.ceil((time.time_ns() + send_time_local) / 2000000.0) + receive_time_server['serverTime']
 
     # Market Data Endpoints
 
