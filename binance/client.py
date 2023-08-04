0e62770f0b801040bb22aece4759ccda61492ab2
david.cheung <kccheung0625@gmail.com>
2023-08-04 15:51:39 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -26,7 +26,7 @@ class BaseClient:
     BASE_API_URLS = [
         # 'https://api.binance.com',
         # 'https://api1.binance.com',
-        'https://api2.binance.com',
+        # 'https://api2.binance.com',
         'https://api3.binance.com',
         'https://api4.binance.com'
         # 'https://api-gcp.binane.com'
