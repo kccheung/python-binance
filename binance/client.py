3c2225a7a0e6b1da1eb687bffa9e7ddcacd70b36
david.cheung <kccheung0625@gmail.com>
2023-08-09 22:15:20 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -7,6 +7,7 @@ import hmac
 import requests
 import time
 import math
+import json
 from operator import itemgetter
 from urllib.parse import urlencode
 
@@ -354,12 +355,13 @@ class BaseClient:
 
         # sort get and post params to match signature order
         if data and method != 'post':
-            # sort post params and remove any arguments with values of None
-            kwargs['data'] = self._order_params2(kwargs['data'])
-            # Remove any arguments with values of None.
-            null_args = [i for i, (key, value) in enumerate(kwargs['data']) if value is None]
-            for i in reversed(null_args):
-                del kwargs['data'][i]
+            # # sort post params and remove any arguments with values of None
+            # kwargs['data'] = self._order_params2(kwargs['data'])
+            # # Remove any arguments with values of None.
+            # null_args = [i for i, (key, value) in enumerate(kwargs['data']) if value is None]
+            # for i in reversed(null_args):
+            #     del kwargs['data'][i]
+            kwargs['data'] = json.dumps(kwargs['data'])
 
         # if get request assign data array to params value for requests lib
         if data and (method == 'get' or signed or force_params):
