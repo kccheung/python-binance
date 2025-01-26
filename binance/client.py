1643f1b5340458df8d4d800c5871e56a7b0fbcdb
david.cheung <kccheung0625@gmail.com>
2025-01-26 15:39:48 +0800

    modified:   binance/client.py

--- binance/client.py
+++ binance/client.py
@@ -71,7 +71,7 @@ class BaseClient:
             if len(api_secret) == 64:
                 self.API_SECRET = api_secret.encode()
                 self._sign = self._hmac
-            elif len(api_secret) > 100:
+            elif len(api_secret) > 200:
                 self.API_SECRET = serialization.load_pem_private_key(api_secret, password=pwd)
                 self._sign = self._rsa
             else:
